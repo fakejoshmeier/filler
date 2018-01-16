83c2acc9d3dfddbf8acb2d9c51117b447fc14522
Josh Meier <josh@jm-debian.home>
2018-01-16 11:46:31 -0800

    Parsing is done, unless I misnamed something, in which case it's not

--- src/parse_map.c
+++ src/parse_map.c
@@ -6,7 +6,7 @@
 /*   By: jmeier <jmeier@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/06 17:33:48 by jmeier            #+#    #+#             */
-/*   Updated: 2018/01/15 19:38:27 by jmeier           ###   ########.fr       */
+/*   Updated: 2018/01/16 11:45:37 by josh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,9 @@ void	get_player(char *str, t_bodo *board)
 	i = -1;
 	while (str[++i] != 'p')
 		;
-	if (ft_isdigit(str[i]))
-		{
-			board->player = str[i] == 1 ? 'o' : 'x';
-			board->prog = 1;
-			return ;
-		}
+	board->player = str[i] == 1 ? 'o' : 'x';
+	board->prog = 1;
+	return ;
 }
 
 void	get_dimensions(char *str, t_bodo *board)
@@ -37,7 +34,20 @@ void	get_dimensions(char *str, t_bodo *board)
 		board->dimx = board->dimx * 10 + (str[i] - '0');
 	while (str[++i] != ':')
 		board->dimy = board->dimy * 10 + (str[i] - '0');
+	board->map = ft_memalloc(sizeof(char *) * board->dimy);
 	board->prog = 2;
+}
+
+void	map_storage(char *str, t_bodo *board)
+{
+	str += 4;
+	ft_strcpy(map[board->pos], str);
+	if (board->pos == board->dimx)
+	{
+		board->prog = 3;
+		return ;
+	}
+	board->++pos;
 }
 
 void	get_piece(char *str, t_bodo *board)
@@ -50,31 +60,16 @@ void	get_piece(char *str, t_bodo *board)
 		board->piece.x = board->piece.x * 10 + (str[i] - '0');
 	while (str[++i] != ':')
 		board->piece.y = board->piece.y * 10 + (str[i] - '0');
+	board->piece.map = ft_memalloc(sizeof(char *) * board->piece.y);
 }
 
 void	get_token(char *str, t_bodo *board)
 {
-	board->piece.map[board->pla] = ft_memalloc(sizeof(char) * board->piece.y);
-	if (board->pla <= board->piece.x)
-		ft_strcpy(board->piece.grid[board->pla], str);
+	ft_strcpy(board->piece.grid[board->pla], str);
 	if (board->pla == board->piece.x)
 	{
 		board->prog = 4;
 		return ;
 	}
 	board->++pla;
-}
-
-void	map_storage(char *str, t_bodo *board)
-{
-	board->map[board->pos] = ft_memalloc(sizeof(char) * board->dimy);
-	str += 4;
-	if (board->pos <= board->dimx)
-		ft_strcpy(map[board->pos], str);
-	if (board->pos == board->dimx)
-	{
-		board->prog = 3;
-		return ;
-	}
-	board->++pos;
 }
